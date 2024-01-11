e724866fde8b23105cc98ef2b19f7f79aa62f303
Uroboros42 <randazzotony1@gmail.com>
2024-01-11 21:18:48 +0100

    Upload

---
+++ script.js
@@ -0,0 +1,40 @@
+function addFadeInAnimation(elementId) {
+    const heading = document.querySelector(elementId);
+    heading.classList.add('fadeInLeft');
+
+    setTimeout(() => {
+        heading.classList.remove('fadeInLeft');
+    }, 1000);
+}
+
+// Seleziona i pulsanti "up" per ciascuna sezione e aggiungi l'evento di click
+const upButtons = document.querySelectorAll('a[href="#Front-End"], a[href="#Back-End"], a[href="#Game Dev"]');
+upButtons.forEach((button) => {
+    button.addEventListener('click', function(event) {
+        // Ottieni l'ID dell'elemento corrispondente al pulsante cliccato
+        const sectionId = event.target.getAttribute('href');
+        addFadeInAnimation(sectionId);
+    });
+});
+
+const barElements = document.querySelectorAll('.elements [class*=bar]');
+const textContents = {
+    'html-bar': 'HTML',
+    'css-bar': 'CSS',
+    'js-bar': 'JavaScript',
+    'php-bar': 'PHP',
+    'mySQL-bar': 'MySQL',
+    'Godot-bar': 'Godot',
+    'UE-bar': 'Unreal Engine',
+    'Blender-bar': 'Blender'
+};
+
+barElements.forEach(bar => {
+    const className = bar.classList[0];
+    const text = document.createElement('div');
+    text.textContent = textContents[className];
+    text.classList.add('bar-text');
+    bar.parentNode.insertBefore(text, bar);
+});
+
+
