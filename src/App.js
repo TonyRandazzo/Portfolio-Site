8b333db2f9d756ec79a477c10f30ef8baecc1d25
Uroboros42 <randazzotony1@gmail.com>
2024-06-12 15:53:07 +0200

    changes

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import Main from './component/Main';
 import Navbar from './component/Navbar';
 import Body from './component/Body';
+import Details from './component/Details';
 
 function App() {
   return (
     <>
       <Navbar/>
       <Main/>
+      <Details/>
       <Body/>
     </>
 
--- src/component/Main.js
+++ src/component/Main.js
@@ -11,7 +11,11 @@ function Main() {
             <div className='col'>
               <p className='animated-text'>FULL STACK DEVELOPER</p>
             </div>
+            <div className='aboutMe'>
             <img className='foto' src={immagine} alt='Portfolio' />
+            <h2>ABOUT ME</h2>
+            <p>I'm a 19 years old very passionate developer. I'm willing to always improve myself and learn new things.</p>
+            </div>
             <div className='col'>
               <p className='bouncing-text'>GAME DEVELOPER</p>
             </div>
